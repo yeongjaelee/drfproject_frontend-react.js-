1314ae0fb809b59ccc97990e0bad85bdf55125ad
yeongjae <yeongjae.blocket@gmail.com>
2022-12-07 19:54:58 +0900

    frontend first commit

---
+++ src/RestAPI.js
@@ -0,0 +1,65 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+function RestAPI(){
+    const [text, setText] = useState([]);
+
+    return (
+        <>
+            <h1>practice Rest API</h1>
+            <div className="btn-primary">
+                <button onClick={()=>{
+                    axios
+                        .post("http://127.0.0.1:8000/review/",{
+                        title: "title", content: "content"
+                    })
+                        .then(function (response){
+                            console.log(response);
+                        })
+                        .catch(function (error){
+                            console.log(error);
+                        });
+                }}
+                        >
+                    POST
+                </button>
+                <button
+                    onClick={() =>{
+                    axios
+                        .get("http://127.0.0.1:8000/review/")
+                        .then((response) =>{
+                            setText([...response.data]);
+                            console.log(response.data);
+                        })
+                        .catch(function(error){
+                        console.log(error);
+                    });
+                }}
+                        >
+                    GET
+                </button>
+            </div>
+            {text.map((e)=>(
+                <div>
+                    {""}
+                    <div className="list">
+                        <span>
+                            {e.id}, {e.title}, {e.content}
+                        </span>
+                        <button
+                            className="btn-delete"
+                            onClick={()=>{
+                                axios.delete(`http://127.0.0.1:8000/review/${e.id}/`);
+                                setText(text.filter((text)=> text.id !==e.id));
+                            }}
+                            >
+                            DELETE
+                        </button>{""}
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default RestAPI;
